de8bfeabc2ce8c9141ae8d36011435ea5df8b1d9
minibug <mrhoontar@gmail.com>
2023-03-04 19:20:59 +0100

    Add files via upload

---
+++ pushshift.js
@@ -0,0 +1,8 @@
+function getMostRecent(){
+	$.getJSON('https://api.pushshift.io/reddit/search/submission?q=reddit&order=desc&before=1667260800&size=1', function(data) {
+		date = data["data"][0]["utc_datetime_str"];
+		document.getElementById("date").innerHTML = date;
+	});
+}
+
+getMostRecent();
