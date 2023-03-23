fefc69069c436d772c0f54f6f9a66b105f6edebb
minibug <mrhoontar@gmail.com>
2023-03-23 22:12:40 +0100

    Update pushshift.js

--- pushshift.js
+++ pushshift.js
@@ -1,6 +1,6 @@
 function getMostRecent(){
 	$.getJSON('https://api.pushshift.io/reddit/search/submission?q=reddit&order=desc&before=1667260800&size=1', function(data) {
-		if (data["data"][0]["created_utc"] == 1609851630) {
+		if (data["data"][0]["created_utc"] == 1610024435) {
 			updateOverride();
 		} else {
 			date = data["data"][0]["utc_datetime_str"];
