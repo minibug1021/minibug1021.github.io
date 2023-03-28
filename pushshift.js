79937b13a960be19f54feba9aa69a29584ab7543
minibug <mrhoontar@gmail.com>
2023-03-28 13:59:32 +0200

    Update pushshift.js

--- pushshift.js
+++ pushshift.js
@@ -3,15 +3,6 @@ function getMostRecent(){
 		date = data["data"][0]["utc_datetime_str"];
 		document.getElementById("date").innerHTML = date;
 		document.title = document.title + ", " + date;
-		updateOverride();
-	});
-}
-
-function updateOverride(){
-	$.getJSON('https://api.pushshift.io/reddit/search/submission?q=reddit&order=desc&before=1609459200&size=1', function(data) {
-		date = data["data"][0]["utc_datetime_str"];
-		document.getElementById("datetwo").innerHTML = date;
-		document.title = date;
 	});
 }
 
